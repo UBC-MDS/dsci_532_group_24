d57f3ba4fa9c5726922233109ca78849beac4bf8
Junghoo Kim <jkim222383@gmail.com>
2021-02-04 21:31:39 -0800

    Update layouts.

--- src/app.py
+++ src/app.py
@@ -556,6 +556,7 @@ app.layout = dbc.Container(
         )
     ],
     fluid=True,
+    style = {'max-width' : '95%'}
 )
 
 @app.callback(
@@ -743,15 +744,16 @@ def plot_country(year_range, countries, diseases, stat_type):
                         format='.2f'
                     )
                 ],
-            ).properties(title=f"Deaths Per 1000 Children in Each Country between {year_range[0]} and {year_range[1]}")
+            )
+            .properties(title=f"Deaths Per 1000 Children in Each Country between {year_range[0]} and {year_range[1]}")
         )
     return (
-        (year_chart + year_chart.mark_point(size=100).encode(
+        (year_chart + year_chart.mark_point(size=50).encode(
                 fill=alt.Fill(
                     "country", title="Country", sort="-y"
                 ),
         )).properties(
-            width=800, height=500)
+            width=700, height=300)
         .configure_title(fontSize=20)
         .configure_axis(labelFontSize=15, titleFontSize=20)
         .configure_legend(orient="right", labelFontSize=15, titleFontSize=20)
@@ -865,7 +867,7 @@ def plot_disease(year_range, countries, diseases, stat_type):
             ).properties(title=f"Deaths Per 1000 Children from Each Disease between {year_range[0]} and {year_range[1]}")
         )
     return (
-        (year_chart + year_chart.mark_point().encode(
+        (year_chart + year_chart.mark_point(size=50).encode(
                 fill=alt.Fill(
                     "disease", title="Disease", sort="-y"
                 ),
@@ -1137,8 +1139,6 @@ def display_choropleth(year, countries, diseases, stat_type):
             color_continuous_scale=px.colors.sequential.Plasma,
         )
         fig.update_layout(
-            # height=700,
-            # width=600,
             geo_scope="africa",
             margin=dict(l=0, r=0, b=0, t=0),
             coloraxis_colorbar=dict(
@@ -1169,8 +1169,6 @@ def display_choropleth(year, countries, diseases, stat_type):
             color_continuous_scale=px.colors.sequential.Plasma,
         )
         fig.update_layout(
-            # height=700,
-            # width=600,
             geo_scope="africa",
             margin=dict(l=0, r=0, b=0, t=0),
             coloraxis_colorbar=dict(
